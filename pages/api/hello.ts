96c7474312061fd08033570c83de6479e807d6b9
Yuto Watanabe <yuto.w51942@gmail.com>
2022-04-29 15:59:28 +0900

    grs導入

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {NextApiRequest, NextApiResponse} from 'next';
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
+/**
+ *
+ * @param {NextApiRequest} req - req
+ * @param {NextApiResponse<Data>} res - res
+ */
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({name: 'John Doe'});
 }

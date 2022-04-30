34284dd701f0dd936b6add5cd2143180ab0fcabf
Yuto Watanabe <yuto.w51942@gmail.com>
2022-05-01 00:40:50 +0900

    gaとogp設定

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
+![image](./frontend/public/ogp.png)
+
 # ふぁいんど！
 
-待ち合わせがスムーズになるWebアプリ
+待ち合わせ相手が楽に見つかるWebアプリ
+
+## TL;DR
+
+- 相手との距離と方角が表示される
 
 ## LICENSE
 
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,26 @@
 import {ChakraProvider} from '@chakra-ui/react';
 import type {AppProps} from 'next/app';
+import {useRouter} from 'next/router';
+import React from 'react';
 import {RecoilRoot} from 'recoil';
 import Base from '../components/Common/Base';
+import {pageview, GA_TRACKING_ID} from '../utils/gtag';
 
 const MyApp = ({Component, pageProps}: AppProps) => {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (!GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <RecoilRoot>
       <ChakraProvider>

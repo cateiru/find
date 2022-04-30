26807a4a3899fcc211d472a87a69dda43c8bfb7f
Yuto Watanabe <yuto.w51942@gmail.com>
2022-04-30 16:29:44 +0900

    バックエンド枠つくった

---
+++ backend/index.ts
@@ -0,0 +1,21 @@
+import http from 'http';
+import express from 'express';
+import {Server} from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+io.on('connection', socket => {
+  console.log('a user connected');
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});

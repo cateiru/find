e2e3bdf56e6fb2ff2e3b631d4383c32ddead8267
Yuto Watanabe <yuto.w51942@gmail.com>
2022-05-05 14:35:16 +0900

    debugをlocalstorageに保存するようにした

--- frontend/utils/atom.ts
+++ frontend/utils/atom.ts
@@ -1,6 +1,24 @@
-import {atom} from 'recoil';
+import {atom, AtomEffect} from 'recoil';
+
+const localStorageEffect =
+  <T>(key: string): AtomEffect<T> =>
+  ({setSelf, onSet}) => {
+    if (process.browser) {
+      const savedValue = localStorage.getItem(key);
+      if (savedValue !== null) {
+        setSelf(JSON.parse(savedValue));
+      }
+
+      onSet((newValue, _, isReset) => {
+        isReset
+          ? localStorage.removeItem(key)
+          : localStorage.setItem(key, JSON.stringify(newValue));
+      });
+    }
+  };
 
 export const DebugModeState = atom({
   key: 'DebugModeState',
   default: false,
+  effects: [localStorageEffect<boolean>('debug_mode')],
 });
